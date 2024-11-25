702d7d632f84408f73b536659900b6b77cff1315
Андрей Трепалин <Hitmanki@yandex.ru>
2024-11-26 01:10:32 +0400

    The logic is implemented when only one time, and one user can click on the Share button

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -11,6 +11,6 @@ router.put("/dislikePost/:postId", verifyToken, dislikePost);
 router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
 router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 router.put("/:postId/incrementViews", incrementViews);
-router.put("/:postId/share", sharePost);
+router.put("/:postId/share", verifyToken, sharePost);
 
 export default router;
