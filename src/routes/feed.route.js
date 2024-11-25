bea79d04b6a81c6470b7b6b35d8088bef6a729e7
Андрей Трепалин <Hitmanki@yandex.ru>
2024-11-25 14:38:57 +0400

    Removed files related to feed

--- src/routes/feed.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getfeeds } from "../controllers/feed.controller";
-
-const router = express.Router();
-
-router.get("/getfeeds", getfeeds);
-
-export default router;
